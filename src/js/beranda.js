522cf0d7b51fed9b563a3bb25f35a5cef8f559c9
saria4558 <saria4558@gmail.com>
2025-06-11 08:06:44 +0700

    fiks logout from beranda

--- src/js/beranda.js
+++ src/js/beranda.js
@@ -130,4 +130,22 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("rekomendasi").addEventListener("click", function() {
     window.location.href = "rekomendasi.html";
     });
+
+    document.getElementById('logoutbtn').addEventListener('click', function (e) {
+    e.preventDefault();
+    localStorage.removeItem('token'); // ganti 'token' sesuai nama penyimpananmu
+    sessionStorage.removeItem('token');
+
+    fetch(`${BASE_API_URL}/logout`, {  
+        method: 'POST'
+    }).then(res => res.json()).then(data => {
+        console.log(data.message); 
+       
+        window.location.href = 'home.html'; 
+    }).catch(err => {
+        console.error('Logout error:', err);
+        window.location.href = 'home.html'
+    });
+});
+
 });
